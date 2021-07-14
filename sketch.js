9451402059257c49e5d7aacabc236e1de8a1c797
Chetrithasai <87260107+Chetrithasai@users.noreply.github.com>
2021-07-14 07:47:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+/*churos Recipe
+
+ingridients;
+4 cups(oil);
+1 cup(water);
+1 cup(flour);
+1/2 tsp(baking powder);
+1/4 tsp(salt);
+1(egg);
+2 tbsp(coconut oil);
+2 tbsp(sugar);
+3 tbsp(cinnamon);
+4 cups(sugar(to roll the churros in));
+
+1st Step();
+bowl1 = sugar+flour+baking powder+salt(whisk);
+saucepan = water+coconut oil(boil in high heat);
+add the water+coconut oil mixture into (bowl1);
+whisk together(until batter is air and fluffy);
+bowl1 = egg(whisk until thick texture);
+pipe the batter into a piping bag(pin 2D);
+pipe out vertically in oil(180oC);
+fry in oil until golden brown;
+
+after cooling down
+roll in the sugar and cinnamon mixture(optional);
+
+enjoy
+
+*/
